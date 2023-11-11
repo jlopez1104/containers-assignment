92cacb1a7c7cba7bd286456ecd387350f6b03720
jlopez1104 <145376782+jlopez1104@users.noreply.github.com>
2023-11-11 05:04:00 +0000

    DONE

--- practice5.py
+++ practice5.py
@@ -1,17 +1,22 @@
 favorite_foods = {}
 for i in range(1, 5):
-    food = input(f"Enter your favorite food #: ")
+    food = input(f"Enter your favorite food : ")
     favorite_foods[i] = food
 print(favorite_foods)
 choice = input("would you like to remove food?(y/n): ")
-if choice == 'y':
-    item_name = input('please enter an item in list you wish to remove: ')
-    if item_name in favorite_foods:
-        del favorite_foods[item_name]
-        print('item successfully removed')
+while True:
+    if choice == 'y':
+        item_name = input('please enter an item in list you wish to remove: ')
+        if item_name in favorite_foods:
+            del favorite_foods[item_name]
+            print('item successfully removed')
+            print("list after removal:")
+            print(favorite_foods)
+        else:
+            print('item you have selected is not currently in list')
+    elif choice == 'n':
+        print(favorite_foods)
+        break
     else:
-        print('item you have selected is not currently in list')
-sorted_foods = dict(sorted(favorite_foods.items()))
-
-print("list after removal:")
-print(favorite_foods)
+        print('please select correct answer')
+        break
